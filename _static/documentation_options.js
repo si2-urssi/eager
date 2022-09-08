b988b94f9adef229ea970a5e311708a5c0976356
evamaxfield <evamaxfieldbrown@gmail.com>
2022-09-08 06:44:10 +0000

    Deploying to gh-pages from @ PugetSoundClinic-PIT/soft-search@a9772cd855de771d6f890b60621304b2bda31147 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+gf501250',
+    VERSION: '0.1.dev1+ga9772cd',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
