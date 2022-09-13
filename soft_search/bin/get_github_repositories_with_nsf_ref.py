39ef3dfa7db1c1e2ca471159ab275682e00e568c
Eva Maxfield Brown <evamaxfieldbrown@gmail.com>
2022-09-13 10:30:47 -0700

    Lint and format

--- soft_search/bin/get_github_repositories_with_nsf_ref.py
+++ soft_search/bin/get_github_repositories_with_nsf_ref.py
@@ -33,6 +33,7 @@ BATCH_SIZE = 10
 
 ###############################################################################
 
+
 class Args(argparse.Namespace):
     def __init__(self) -> None:
         self.__parse()
@@ -40,9 +41,7 @@ class Args(argparse.Namespace):
     def __parse(self) -> None:
         p = argparse.ArgumentParser(
             prog="get-github-repositories-with-nsf-ref",
-            description=(
-                "Search for GitHub repositories which reference NSF Awards."
-            ),
+            description=("Search for GitHub repositories which reference NSF Awards."),
         )
         p.add_argument(
             "-o",
@@ -88,7 +87,9 @@ class Args(argparse.Namespace):
         )
         p.parse_args(namespace=self)
 
+
 ###############################################################################
+
 
 def main() -> None:
     # Get args
@@ -107,111 +108,145 @@ def main() -> None:
     )
     log = logging.getLogger(__name__)
 
-    # Determine token / api
-    if args.token is None:
-        load_dotenv()
-        api = GhApi()
-    else:
-        api = GhApi(token=args.token)
+    try:
+        # Determine token / api
+        if args.token is None:
+            load_dotenv()
+            api = GhApi()
+        else:
+            api = GhApi(token=args.token)
 
-    # Clean
-    if args.clean:
-        if args.outdir.exists():
-            shutil.rmtree(args.outdir)
-    
-    # Make dir if needed
-    args.outdir.mkdir(parents=True, exist_ok=True)
+        # Clean
+        if args.clean:
+            if args.outdir.exists():
+                shutil.rmtree(args.outdir)
 
-    # Get all results for each term
-    query_start_time = time.time()
-    for query, page in SEARCH_QUERIES_START_PAGE.items():
-        log.info(f"Beginning page requests for: '{query}'")
-        complete_query = f'"{query}" filename:README.md'
+        # Make dir if needed
+        args.outdir.mkdir(parents=True, exist_ok=True)
 
-        # Get initial
-        all_gathered = False
-        while not all_gathered:
-            try:
-                log.debug(f"Querying: '{complete_query}', Page: {page}")
-                page_results = api("/search/code", "GET ", query=dict(q=complete_query, per_page=BATCH_SIZE, page=page))
-                total_count = page_results["total_count"]
-                real_count = total_count if total_count < 1000 else 1000
-                items_returned = page_results["items"]
+        # Get all results for each term
+        query_start_time = time.time()
+        for query, page in SEARCH_QUERIES_START_PAGE.items():
+            log.info(f"Beginning page requests for: '{query}'")
+            complete_query = f'"{query}" filename:README.md'
 
-                # Unpack results
-                results = []
-                for item in items_returned:
-                    repo_details = item["repository"]
-                    repo_name = repo_details["name"]
-                    owner_details = repo_details["owner"]
-                    owner_name = owner_details["login"]
-                    full_name = f"{owner_name}/{repo_name}"
+            # Get initial
+            all_gathered = False
+            while not all_gathered:
+                try:
+                    log.debug(f"Querying: '{complete_query}', Page: {page}")
+                    page_results = api(
+                        "/search/code",
+                        "GET ",
+                        query=dict(
+                            q=complete_query,
+                            per_page=BATCH_SIZE,
+                            page=page,
+                        ),
+                    )
+                    total_count = page_results["total_count"]
+                    real_count = total_count if total_count < 1000 else 1000
+                    items_returned = page_results["items"]
 
-                    # Get languages
-                    languages = api(f"/repos/{full_name}/languages")
+                    # Unpack results
+                    results = []
+                    for item in items_returned:
+                        repo_details = item["repository"]
+                        repo_name = repo_details["name"]
+                        owner_details = repo_details["owner"]
+                        owner_name = owner_details["login"]
+                        full_name = f"{owner_name}/{repo_name}"
 
-                    # Get latest commit datetime
-                    commits = api(f"/repos/{full_name}/commits")
-                    most_recent_commit = commits[0]["commit"]
-                    most_recent_committer = most_recent_commit["committer"]
-                    most_recent_committer_name = most_recent_committer["name"]
-                    most_recent_committer_email = most_recent_committer["email"]
-                    most_recent_commit_dt = datetime.fromisoformat(
-                        # We remove last character because it is 'Z' for "Zulu"
-                        # Datetimes are naturally UTC/Zulu
-                        most_recent_committer["date"][:-1]
+                        # Get languages
+                        languages = api(f"/repos/{full_name}/languages")
+
+                        # Get latest commit datetime
+                        commits = api(f"/repos/{full_name}/commits")
+                        most_recent_commit = commits[0]["commit"]
+                        most_recent_committer = most_recent_commit["committer"]
+                        most_recent_committer_name = most_recent_committer["name"]
+                        most_recent_committer_email = most_recent_committer["email"]
+                        most_recent_commit_dt = datetime.fromisoformat(
+                            # We remove last character because it is 'Z' for "Zulu"
+                            # Datetimes are naturally UTC/Zulu
+                            most_recent_committer["date"][:-1]
+                        )
+
+                        # Append this result to all results
+                        results.append(
+                            {
+                                "owner": owner_name,
+                                "name": repo_name,
+                                "link": f"https://github.com/{full_name}",
+                                "languages": "; ".join(languages.keys()),
+                                "most_recent_committer_name": (
+                                    most_recent_committer_name
+                                ),
+                                "most_recent_committer_email": (
+                                    most_recent_committer_email
+                                ),
+                                "most_recent_commit_datetime": (
+                                    most_recent_commit_dt.isoformat()
+                                ),
+                                "most_recent_commit_timestamp": (
+                                    most_recent_commit_dt.timestamp()
+                                ),
+                                "query": query,
+                            }
+                        )
+
+                    # Store partial results
+                    if len(results) != 0:
+                        save_name = f"{query.lower().replace(' ', '_')}-page_{page}.csv"
+                        pd.DataFrame(results).to_csv(
+                            args.outdir / save_name,
+                            index=False,
+                        )
+
+                    # Increase page and keep going
+                    page += 1
+
+                    # Wait to avoid rate limiting
+                    log.debug("Sleeping for one minute...")
+                    time.sleep(60)
+
+                    # Update time estimate
+                    batch_time = time.time()
+                    seconds_diff = batch_time - query_start_time
+                    seconds_diff_per_page = seconds_diff / page
+                    total_pages_required = real_count / BATCH_SIZE
+                    remaining_pages = total_pages_required - page
+                    estimated_remaining_seconds = (
+                        seconds_diff_per_page * remaining_pages
+                    )
+                    estimated_remained_pages = remaining_pages
+                    log.info(
+                        f"Remaining pages: {estimated_remained_pages} "
+                        f"(of {total_pages_required} -- "
+                        f"est. {timedelta(seconds=estimated_remaining_seconds)})"
                     )
 
-                    # Append this result to all results
-                    results.append(
-                        {
-                            "owner": owner_name,
-                            "name": repo_name,
-                            "link": f"https://github.com/{full_name}",
-                            "languages": "; ".join(languages.keys()),
-                            "most_recent_committer_name": most_recent_committer_name,
-                            "most_recent_committer_email": most_recent_committer_email,
-                            "most_recent_commit_datetime": most_recent_commit_dt.isoformat(),
-                            "most_recent_commit_timestamp": most_recent_commit_dt.timestamp(), 
-                            "query": query,
-                        }
-                    )
+                    # Break because we are done
+                    # Stop at 1000 results because GitHub limits search
+                    # https://github.com/PyGithub/PyGithub/issues/1072#issuecomment-499211486
+                    if len(items_returned) == 0 or page * BATCH_SIZE >= 1000:
+                        log.info("Reached GitHub max search results.")
+                        break
 
-                # Store partial results
-                if len(results) != 0:
-                    save_name = f"{query.lower().replace(' ', '_')}-page_{page}.csv"
-                    pd.DataFrame(results).to_csv(
-                        args.outdir / save_name,
-                        index=False,
-                    )
+                except HTTP4xxClientError as e:
+                    log.error(f"Caught exception: {e}")
 
-                # Increase page and keep going
-                page += 1
+    except Exception as e:
+        log.error("=============================================")
+        log.error("\n\n" + traceback.format_exc())
+        log.error("=============================================")
+        log.error("\n\n" + str(e) + "\n")
+        log.error("=============================================")
+        sys.exit(1)
 
-                # Wait to avoid rate limiting
-                log.debug("Sleeping for one minute...")
-                time.sleep(60)
 
-                # Update time estimate
-                batch_time = time.time()
-                seconds_diff = batch_time - query_start_time
-                seconds_diff_per_page = seconds_diff / page
-                total_pages_required = real_count / BATCH_SIZE
-                remaining_pages = total_pages_required - page
-                estimated_remaining_seconds = seconds_diff_per_page * remaining_pages
-                estimated_remained_pages = remaining_pages
-                log.info(
-                    f"Remaining pages: {estimated_remained_pages} "
-                    f"(of {total_pages_required} -- "
-                    f"est. {timedelta(seconds=estimated_remaining_seconds)})"
-                )
+###############################################################################
+# Allow caller to directly run this module (usually in development scenarios)
 
-                # Break because we are done
-                # Stop at 1000 results because GitHub limits search
-                # https://github.com/PyGithub/PyGithub/issues/1072#issuecomment-499211486
-                if len(items_returned) == 0 or page * BATCH_SIZE >= 1000:
-                    log.info("Reached GitHub max search results.")
-                    break
-
-            except HTTP4xxClientError as e:
-                log.error(f"Caught exception: {e}")
+if __name__ == "__main__":
+    main()
