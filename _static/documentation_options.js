4d47c48e820562e52bc91d7a5677dd26be8d9e48
evamaxfield <evamaxfieldbrown@gmail.com>
2022-10-24 19:01:49 +0000

    Deploying to gh-pages from @ si2-urssi/eager@a83dad677eb1c36bb7821886ee55a3cf9ab94871 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g20e1d67',
+    VERSION: '0.1.dev1+ga83dad6',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
