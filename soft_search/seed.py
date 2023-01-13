1d98600aebcd4c8cea14d3d9dc814fc02bc51f3e
Eva Maxfield Brown <evamaxfieldbrown@gmail.com>
2023-01-12 16:28:31 -0800

    Add global seed function

---
+++ soft_search/seed.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import random
+
+import numpy as np
+import torch
+
+###############################################################################
+
+
+def set_seed(seed: int = 0) -> None:
+    # Set a bunch of seeds for reproducibility
+    torch.manual_seed(seed)
+    random.seed(seed)
+    np.random.seed(seed)
