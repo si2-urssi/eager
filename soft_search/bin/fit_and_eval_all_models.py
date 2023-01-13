e7eea977584e6d654bafe20ad18e3c792216c5ce
Eva Maxfield Brown <evamaxfieldbrown@gmail.com>
2023-01-12 16:29:43 -0800

    Write func and bin to train and eval all models and compare

---
+++ soft_search/bin/fit_and_eval_all_models.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import logging
+import sys
+import traceback
+
+from soft_search.label.model_selection import fit_and_eval_all_models
+
+###############################################################################
+
+class Args(argparse.Namespace):
+    def __init__(self) -> None:
+        self.__parse()
+
+    def __parse(self) -> None:
+        p = argparse.ArgumentParser(
+            prog="fit-and-eval-all-soft-search-2022-models",
+            description=(
+                "Train and evaluate all the different models with "
+                "the soft search 2022 dataset."
+            ),
+        )
+        p.add_argument(
+            "-t",
+            "--test-size",
+            dest="test_size",
+            type=float,
+            default=0.2,
+            help="Test size to use for data splits."
+        )
+        p.add_argument(
+            "-s",
+            "--seed",
+            type=int,
+            default=0,
+            help="Random seed."
+        )
+        p.add_argument(
+            "--debug",
+            dest="debug",
+            action="store_true",
+            help="Run with debug logging.",
+        )
+        p.parse_args(namespace=self)
+
+
+###############################################################################
+
+
+def main() -> None:
+    # Get args
+    args = Args()
+
+    # Determine log level
+    if args.debug:
+        log_level = logging.DEBUG
+    else:
+        log_level = logging.INFO
+
+    # Setup logging
+    logging.basicConfig(
+        level=log_level,
+        format="[%(levelname)4s: %(module)s:%(lineno)4s %(asctime)s] %(message)s",
+    )
+    log = logging.getLogger(__name__)
+
+    # Try training and storage
+    try:
+        results = fit_and_eval_all_models(args.test_size, args.seed)
+        print(results)
+    except Exception as e:
+        log.error("=============================================")
+        log.error("\n\n" + traceback.format_exc())
+        log.error("=============================================")
+        log.error("\n\n" + str(e) + "\n")
+        log.error("=============================================")
+        sys.exit(1)
+
+
+###############################################################################
+# Allow caller to directly run this module (usually in development scenarios)
+
+if __name__ == "__main__":
+    main()
---
+++ soft_search/label/model_selection.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+from ..data.soft_search_2022 import load_soft_search_2022, SoftSearch2022DatasetFields
+from . import regex, semantic_logit, tfidf_logit, transformer
+from ..seed import set_seed
+
+###############################################################################
+
+def fit_and_eval_all_models(test_size: float = 0.2, seed: int = 0) -> pd.DataFrame:
+    # Set global seed
+    set_seed(seed)
+
+    # Load core data
+    data = load_soft_search_2022()
+
+    # Split the data
+    train_df, test_df = train_test_split(
+        data,
+        test_size=test_size,
+        stratify=data[SoftSearch2022DatasetFields.label],
+    )
+
+    # Run each model
+    regex_metrics = regex.train(test_df)
+    _, _, _, tfidf_logit_metrics = tfidf_logit.train(
+        train_df=train_df,
+        test_df=test_df,
+    )
+    _, _, _, semantic_logit_metrics = semantic_logit.train(
+        train_df=train_df,
+        test_df=test_df,
+    )
+    _, _, _, transformer_metrics = transformer.train(
+        train_df=train_df,
+        test_df=test_df,
+    )
+
+    # Create dataframe with metrics
+    return pd.DataFrame([
+        regex_metrics.to_dict(),
+        tfidf_logit_metrics.to_dict(),
+        semantic_logit_metrics.to_dict(),
+        transformer_metrics.to_dict(),
+    ]).sort_values(by="f1", ascending=False)
